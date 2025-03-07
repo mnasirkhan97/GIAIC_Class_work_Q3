fe1680da14fa9c7e745934d736d4bcdae78f21db
M Nasir Khan <immnk97@gmail.com>
2025-03-08 00:02:09 +0500

    add initial Python script demonstrating variable declaration, data types, and type conversion

---
+++ class_01/app.py
@@ -0,0 +1,42 @@
+# Variables
+x = 10
+print(x)
+
+# Multiple variables
+a, b, c, d = 12, 13, 14, 15
+print(a, b, c, d)
+
+# Data types Int, float, string
+x1 = 5
+type(x1)
+x2 = 5.2
+type(x2)
+x3 = "5"
+type(x3)
+
+
+# change type
+c = 3.4
+type(c)
+
+d = int(c)
+type(d)
+
+e = 1.7
+type(e)
+
+f = int(e)
+type(f)
+
+print(e)
+print(f)
+
+#  Booleans
+t1 = True
+type(t1)
+
+f1 = False
+type(f1)
+
+print(t1)
+print(f1)
