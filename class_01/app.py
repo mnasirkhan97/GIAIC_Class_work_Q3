6ab9bb4d71780c179aa603d523dbd0d591009975
M Nasir Khan <immnk97@gmail.com>
2025-03-08 00:16:11 +0500

    add examples of string concatenation and arithmetic operations in Python

--- class_01/app.py
+++ class_01/app.py
@@ -40,3 +40,37 @@ type(f1)
 
 print(t1)
 print(f1)
+
+# str methods
+str_name = "hello" "Ahmad"
+print(str_name)
+
+str_name1 = "hello"
+str_name2 = "Ahmad"
+print(str_name1 + str_name2)
+
+# example
+price = 50
+
+print("The price is" + price)  # error on this line
+print("The price is", price)  # correct
+
+
+# Arithematic Operators
+plus = 4 + 6
+print(plus)
+
+minus = 12 - 6
+print(minus)
+
+multiply = 30 * 6
+print(multiply)
+
+divide = 66 / 6
+print(divide)
+
+power = 2 ** 3
+print(power)
+
+modulus = 10 % 3
+print(modulus)
