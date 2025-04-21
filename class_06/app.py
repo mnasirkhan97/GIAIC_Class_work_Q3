be41f581c5030c9f463a71d3e6ab125372273b7e
M Nasir Khan <immnk97@gmail.com>
2025-04-21 22:15:33 +0500

    class 06

---
+++ class_06/app.py
@@ -0,0 +1,22 @@
+print("Class ====> 06")
+
+# pip install requests
+import requests
+
+# List 
+
+months: list = ['January' , 'Febuary', 'March', 'April', 'May', 'June', 'July', 'Aug']
+months[0] = 'Dec'
+print(months)
+print(months[0:3])
+# Slicing List 
+print(months[3:6])
+
+print(months[3:])
+
+# # Negative Indexing
+print(months[-3:-1])
+
+
+
+
