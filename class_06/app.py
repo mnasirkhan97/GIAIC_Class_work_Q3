9ed8966cb9e2d737c792010d2a6158c04c4c827e
M Nasir Khan <immnk97@gmail.com>
2025-04-21 22:15:59 +0500

    class 06

--- class_06/app.py
+++ class_06/app.py
@@ -20,3 +20,31 @@ print(months[-3:-1])
 
 
 
+#====> Tuples
+numbers: tuple = (12,93,-21,15,27,57)
+# numbers[0] = 813 // Error
+
+print(numbers[-4: -1])
+print(numbers[0:3])
+
+# Tuples
+teachers_names: tuple[str] = ('Ali Jawwad', 'Haseeb', 'Ahmed')
+print(teachers_names)
+print(type(teachers_names))
+
+# tuple to list conversion
+tuple_to_list = list(teachers_names)
+print(tuple_to_list)
+print(type(tuple_to_list))
+
+# Adding elem to list
+print(tuple_to_list.append('Sir Ameen'))
+print(tuple_to_list)
+
+# Converting the list back to tuple
+list_to_tuple = tuple(tuple_to_list)
+print(list_to_tuple)
+print(type(list_to_tuple))
+
+
+
